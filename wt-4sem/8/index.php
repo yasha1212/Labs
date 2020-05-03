520f3bf7de5e5a5cc8d207418f2c6be749237bf9
yasha1212 <doenglish1@mail.ru>
2020-05-03 15:37:20 +0300

    Implement statistics for day

--- wt-4sem/8/index.php
+++ wt-4sem/8/index.php
@@ -6,12 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet">
     <title>Laba8</title>
+    <script>
+        window.onload = function () {
+            var chart = new CanvasJS.Chart("chartDay", {
+                title: {text: "Суточная активность"},
+                animationEnabled: true,
+                width: 900,
+                axisY: {title: "Посещаемость, %"},
+                data: [{type: "line", dataPoints: <?php echo json_encode(getDayInfo(), JSON_NUMERIC_CHECK); ?>}]
+            });
+            chart.render();
+        }
+    </script>
 </head>
 <body>
     <div class="wrapper">
         <div class="header">MyWebsite</div>
         <div class="description"><?php echo handleVisit(); ?></div>
-        <div class="charts">
-        </div>
     </div>
+    <div class="charts">
+        <div id="chartDay"></div>
+    </div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
--- wt-4sem/8/script.php
+++ wt-4sem/8/script.php
@@ -3,6 +3,8 @@
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'root');
     define('DB_NAME', 'lab_db');
+    define('CONNECT_ERR', 0);
+    define('QUERY_ERR', 0);
 
     function handleVisit() {
         $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -16,7 +18,46 @@
         return 'Ошибка: Не удалось установить соединение с БД!<br>' . $mysqli->error;
     }
 
-    function getDayInfo() {}
+    function getTable() {
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if($mysqli->connect_errno) {
+            return CONNECT_ERR;
+        }
+        $query = 'SELECT * FROM activity';
+        return $mysqli->query($query) ? $mysqli->query($query) : QUERY_ERR;    
+    }
+
+    function transformArray($dataPoints) {
+        $sum = 0;
+        foreach($dataPoints as $subArray) {
+            $sum += $subArray['y'];
+        }
+        foreach($dataPoints as $key => $subArray) {
+            $dataPoints[$key]['y'] = round($subArray['y'] / $sum, 2) * 100;
+        }
+        return $dataPoints;
+    }
+
+    function getDayInfo() {
+        $dataPoints = array(
+            array('y' => 0, 'label' => 'Воскресенье'),
+            array('y' => 0, 'label' => 'Понедельник'),
+            array('y' => 0, 'label' => 'Вторник'),
+            array('y' => 0, 'label' => 'Среда'),
+            array('y' => 0, 'label' => 'Четверг'),
+            array('y' => 0, 'label' => 'Пятница'),
+            array('y' => 0, 'label' => 'Суббота')
+        );
+
+        if($result = getTable()) {
+            while($row = $result->fetch_assoc()) {
+                $date = $row['visitDate'];
+                $dataPoints[date('w', strtotime($date))]['y'] += 1;
+            }
+            
+        }
+        return transformArray($dataPoints);
+    }
 
     function getWeekInfo() {}
 
